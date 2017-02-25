b4ab96e8bdf98133b4dc159d2d3181286c0f9767
nick76567 <nick76567@github.com>
2017-02-25 19:00:23 +0800

    add the unfinished search_directory function

--- code/myshell.c
+++ code/myshell.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 #include <unistd.h>
 #include <sys/types.h>
@@ -19,6 +20,8 @@ void show_prompt();
 int get_cmd_line(char *cmdline);
 void process_cmd(char *cmdline);
 
+unsigned int input_arg_handler(char *cmdline, char *program_name, char *option, char *arg_address);
+bool search_directory(char *program_name);
 
 /* The main function implementation */
 int main()
@@ -36,12 +39,8 @@ int main()
 	return 0;
 }
 
-
-void process_cmd(char *cmdline)
-{
-	// printf("%s\n", cmdline);
-    char *token, program_name[MAX_PROGRAM_NAME_LEN], option[MAX_OPTION_LEN];
-    char arg_address[MAX_ARG_ADDRERSS_LEN];
+unsigned int input_arg_handler(char *cmdline, char *program_name, char *option, char *arg_address){
+    char *token;
     unsigned int option_num = 0;
     token = strtok(cmdline, " \t");
     strcpy(program_name, token);
@@ -59,9 +58,30 @@ void process_cmd(char *cmdline)
         token = strtok(NULL, " \t");
     }
 
+    return option_num;
+}
+
+bool search_directory(char *program_name){
+    char buffer[MAX_DIR_LEN];
+    getcwd(buffer, MAX_DIR_LEN);
+
+    opendir(buffer);
+    
+    return false;
+}
+
+void process_cmd(char *cmdline)
+{
+	// printf("%s\n", cmdline);
+    char program_name[MAX_PROGRAM_NAME_LEN], option[MAX_OPTION_LEN];
+    char arg_address[MAX_ARG_ADDRERSS_LEN] = {0};
+    unsigned int option_num = input_arg_handler(cmdline, program_name, option, arg_address);
+    
+/*
     printf("Program_name: %s\n", program_name);
     printf("Option: %s\n", option);
     printf("arg_address: %s\n", arg_address);
+*/
 }
 
 
